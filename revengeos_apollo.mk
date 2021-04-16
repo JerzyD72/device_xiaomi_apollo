21b9c424b6e67584cd62ebddf25469c43ad659be
JerzyD72 <jerzy.dziuba@gmail.com>
2021-04-16 11:33:18 +0200

    apollo:revnegeos bringup

--- revengeos_apollo.mk
+++ revengeos_apollo.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
-PRODUCT_NAME := lineage_apollo
+PRODUCT_NAME := revengeos_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
